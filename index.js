3b853182860cf3057375660d9e2a87dd76a4264e
PrithviSaha <sahaprithvi16@gmail.com>
2024-07-28 02:30:30 +0530

    new

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ const MONGOURI = "mongodb+srv://prithvisaha:abcd@cluster0.tv5vez4.mongodb.net/?r
 
 var app = express();
 
-// view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'ejs');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 try {
   mongoose.connect(MONGOURI);
@@ -24,40 +24,40 @@ try {
   console.log("Not connected to MONGO DB");
 }
 
-// app.use(expressSession({
-//   resave: false,
-//   saveUninitialized: false,
-//   secret: "heyheyehhdd"
-// }));
-// app.use(passport.initialize());
-// app.use(passport.session());
-// passport.serializeUser(usersRouter.serializeUser());
-// passport.deserializeUser(usersRouter.deserializeUser());
+app.use(expressSession({
+  resave: false,
+  saveUninitialized: false,
+  secret: "heyheyehhdd"
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.serializeUser(usersRouter.serializeUser());
+passport.deserializeUser(usersRouter.deserializeUser());
 
-// app.use(logger('dev'));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-// app.use('/users', usersRouter);
+app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
+app.use(function(req, res, next) {
+  next(createError(404));
+});
 
-// // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("running app...");
